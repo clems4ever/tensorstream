f92cacb00f6e2b03010edd6a314f61c255d92401
Clement Michaud <clement.michaud34@gmail.com>
2018-08-11 10:45:55 +0200

    Add pytest as dev dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='TensorStream',
-    version='0.1',
-    packages=['tensorstream',],
-    license='All rights reserved to Clement Michaud',
-    long_description=open('README').read(),
+  name='TensorStream',
+  version='0.1',
+  packages=['tensorstream',],
+  license='All rights reserved to Clement Michaud',
+  long_description=open('README').read(),
+  install_requires=[
+    "tensorflow == 1.5.0",
+  ],
+  extras_require={
+    'dev': ['pytest']
+  }
 )
