3a93f4028dbb276fdfe08e0d6f32f46d4e69f51b
Clement Michaud <clement.michaud34@gmail.com>
2018-08-11 10:14:43 +0200

    Initial setup commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TensorStream',
+    version='0.1',
+    packages=['tensorstream',],
+    license='All rights reserved to Clement Michaud',
+    long_description=open('README').read(),
+)
