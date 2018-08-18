e91c282e4978dfb5950eaecbf1f37cc7fc817d62
Clement Michaud <clement.michaud34@gmail.com>
2018-08-18 23:53:54 +0200

    Fix return operator and add logarithmic return

--- tensorstream/finance/returns.py
+++ tensorstream/finance/returns.py
@@ -4,19 +4,36 @@ import tensorflow as tf
 from tensorstream.common import roll
 from tensorstream.streamable import Streamable
 
-class Returns(Streamable):
+class Return(Streamable):
   def __init__(self, period, dtype=tf.float32, shape=()):
     super().__init__(dtype, shape)
     self.period = period
-    self.initial_state = (0, tf.fill((period,) + shape, math.nan))
+    self.initial_state = (0, tf.fill((self.period,) + shape, math.nan))
 
-  def step(self, value, iteration, old_buffer):
-    old_value = old_buffer[self.period - 1]
-    new_buffer = roll(value, old_buffer)
+  def step(self, value, iteration, prev_buffer):
+    prev_value = prev_buffer[self.period - 1]
+    next_buffer = roll(value, prev_buffer)
 
-    variation = tf.cond(
+    return_ = tf.cond(
       iteration < self.period - 1,
       lambda: tf.fill(self.shape, math.nan),
-      lambda: value / old_value - tf.constant(1.0)
+      lambda: tf.divide(value, prev_value) - tf.constant(1.0)
     )
-    return variation, (iteration + 1, new_buffer)
+    return return_, (iteration + 1, next_buffer)
+
+class LogarithmicReturn(Streamable):
+  def __init__(self, period, dtype=tf.float32, shape=()):
+    super().__init__(dtype, shape)
+    self.period = period
+    self.initial_state = (0, tf.fill((self.period,) + shape, math.nan))
+
+  def step(self, value, iteration, prev_buffer):
+    prev_value = prev_buffer[self.period - 1]
+    next_buffer = roll(value, prev_buffer)
+
+    return_ = tf.cond(
+      iteration < self.period - 1,
+      lambda: tf.fill(self.shape, math.nan),
+      lambda: tf.log(tf.divide(value, prev_value))
+    )
+    return return_, (iteration + 1, next_buffer)
