99421ec9cb2b3d72f9dac3573b030a64786fce62
Clement Michaud <clement.michaud34@gmail.com>
2018-08-19 16:40:20 +0200

    Make volatility more generic by adding return_period as parameter
    Set the default value to 1.

--- tensorstream/finance/volatility.py
+++ tensorstream/finance/volatility.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 from tensorstream.meta.compose import Compose
 from tensorstream.finance.moving_standard_deviation import MovingStandardDeviation
-from tensorstream.finance.returns import Return
+from tensorstream.finance.returns import LogarithmicReturn
 
-def Volatility(period, dtype=tf.float32, shape=()):
+def Volatility(moving_period, return_period=1, dtype=tf.float32, shape=()):
   return Compose(
-    MovingStandardDeviation(period=period, dtype=dtype, shape=shape),
-    Return(period=1, dtype=dtype, shape=shape)
+    MovingStandardDeviation(period=moving_period, dtype=dtype, shape=shape),
+    LogarithmicReturn(period=return_period, dtype=dtype, shape=shape)
   )
