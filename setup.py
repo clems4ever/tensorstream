e1f05ec023506d7109f92247c26116d7a4ccd23f
Clement Michaud <clement.michaud34@gmail.com>
2018-09-26 00:06:05 +0200

    Add all packages in setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup
 
 setup(
   name='TensorStream',
-  version='0.1',
-  packages=['tensorstream'],
+  version='0.0.1',
+  packages=[
+    'tensorstream',
+    'tensorstream.common',
+    'tensorstream.finance',
+    'tensorstream.helpers',
+    'tensorstream.meta',
+  ],
   license='All rights reserved to Clement Michaud',
   long_description=open('README').read(),
   install_requires=[
-    "tensorflow == 1.5.0",
+    "tensorflow==1.5.0",
     "pandas==0.23.4"
   ],
   extras_require={
