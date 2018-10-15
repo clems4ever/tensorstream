a1ccaa275fc75a63cb8d7e1570c50f39452cce82
Clement Michaud <clement.michaud34@gmail.com>
2018-10-15 21:24:34 +0200

    Bump version to 0.0.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='tensorstream',
-  version='0.0.2',
+  version='0.0.3',
   author='Clement Michaud',
   author_email='clement.michaud34@gmail.com',
   license='All rights reserved to Clement Michaud',
