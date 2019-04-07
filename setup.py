1d0c3e7d6bc05deadde5aee1794d46bc8d166cd2
Clement Michaud <c.michaud@criteo.com>
2019-04-07 16:42:54 +0200

    Bump version of tensorflow, pandas and pytest.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
   name='tensorstream',
-  version='0.0.3',
+  version='0.0.4',
   author='Clement Michaud',
   author_email='clement.michaud34@gmail.com',
   license='All rights reserved to Clement Michaud',
 
   packages=find_packages(exclude=['*_spec.py']),
-  long_description=open('README').read(),
+  long_description=open('README.md').read(),
   install_requires=[
-    "tensorflow==1.5.0",
-    "pandas==0.23.4"
+    "tensorflow==1.13.1",
+    "pandas==0.24.2"
   ]
 )
